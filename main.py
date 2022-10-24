1ceb907c43eb65b45afe1874b2fab4af8cf4ac0d
Titulin Alexander <tito-1984@mail.ru>
2022-10-24 16:15:18 +0300

    Add main.py, text.txt, yadisk.py, .gitignore

---
+++ main.py
@@ -0,0 +1,26 @@
+# import requests
+# from pprint import pprint
+
+url = 'https://akabab.github.io/superhero-api/api/all.json'
+response = requests.get(url)
+hero_names = ['Hulk', 'Captain America', 'Thanos']
+all_heroes = response.json()
+our_heroes = filter(lambda hero: hero['name'] in hero_names, all_heroes)
+intel = max({hero['name']: hero['powerstats']['intelligence'] for hero in our_heroes})
+print(f'Самый умный супергерой {intel}')
+
+
+from yadisk import YandexDisk
+
+if __name__ == '__main__':
+    with open(r'd:/New/HW requests/yankey.txt') as file:
+        TOKEN = file.read()
+    with open(r'd:/New/HW requests/text.txt') as file:
+        disk_file_path = file.read()
+    yadisk = YandexDisk(token=TOKEN)
+    yadisk.get_files_list()
+    yadisk._get_upload_link(disk_file_path)
+    yadisk.upload_file_to_disk('/new/text.txt', 'text.txt')
+    
+    
+    
---
+++ yadisk.py
@@ -0,0 +1,34 @@
+from pprint import pprint
+from wsgiref import headers
+import requests
+
+class YandexDisk:
+    host = 'https://cloud-api.yandex.net'
+    def __init__(self, token):
+        self.token = token
+    
+    def get_headers(self):
+        return {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
+    
+    def get_files_list(self):
+        url = f'{self.host}/v1/disk/resources/files'
+        headers = self.get_headers()
+        params = {'fields': 'name'}
+        response = requests.get(url, headers=headers, params=params)
+        pprint(response.json())
+    
+    def _get_upload_link(self, disk_file_path):
+        upload_url = f'{self.host}/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': disk_file_path, 'overwrite': True}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+    
+    def upload_file_to_disk(self, disk_file_path, filename):
+        href = self._get_upload_link(disk_file_path=disk_file_path).get('href', '')
+        headers = self.get_headers()
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print('Success') 
